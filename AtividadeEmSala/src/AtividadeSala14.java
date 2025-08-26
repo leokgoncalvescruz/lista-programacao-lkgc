92b2303fd0beb74eef287ba8c95fcb0193034376
leokgoncalvescruz <leonardokeirrison7@gmail.com>
2025-08-25 22:03:00 -0300

    Atividades em Sala

---
+++ AtividadeEmSala/src/AtividadeSala14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AtividadeSala14 {
+    public static int soma(int x, int y) {
+        return x + y;
+    }
+    public static void imprimir(int valor){
+        System.out.println(valor);
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(soma(20, 35));
+        
+    }
+}
+
+
+
+
+
