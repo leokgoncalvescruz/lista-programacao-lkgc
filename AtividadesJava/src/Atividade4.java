3bfa9a6b15d0958c04c31f053669b9e553a44376
leokgoncalvescruz <leonardokeirrison7@gmail.com>
2025-08-28 19:51:00 -0300

    Atividade4 resolvida

---
+++ AtividadesJava/src/Atividade4.java
@@ -0,0 +1,8 @@
+public class Atividade4 {
+    public static void main(String[] args) {
+        final double PI = 3.1415;
+        int raio = 5;
+        System.out.println("O valor de Pi é: "+PI);
+        System.out.println("O valor de raio é: "+raio);
+    }
+}
