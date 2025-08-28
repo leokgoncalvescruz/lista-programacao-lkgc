926f51b4d27c6e57c0edfa6417915c5c4dbbcbfd
leokgoncalvescruz <leonardokeirrison7@gmail.com>
2025-08-28 19:56:45 -0300

    Atividade 5 resolvida

---
+++ AtividadesJava/src/Atividade5.java
@@ -0,0 +1,8 @@
+public class Atividade5 {
+    public static void main(String[] args) {
+        String curso = "Programação com Java e C#"; int cargaHoraria = 360;
+        System.out.println("Curso: "+curso + ", Carga Horária: "+cargaHoraria+ " Horas");
+
+
+    }
+}
