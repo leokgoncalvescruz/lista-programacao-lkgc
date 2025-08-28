7907037a46a6316a63b549c23ab1684fcc1cdc20
leokgoncalvescruz <leonardokeirrison7@gmail.com>
2025-08-28 19:31:05 -0300

    Atividade 2 resolvida

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,10 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        int dia = 3;
+        String mes = "Setembro";
+        int ano = 2008;
+        System.out.println("Dia: "+dia);
+        System.out.println("Mês: "+mes);
+        System.out.println("Ano: "+ano);
+    }
+}
