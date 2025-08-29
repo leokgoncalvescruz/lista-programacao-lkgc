55e8c710a6bdd89956b54498e18a73b572672d96
leokgoncalvescruz <leonardokeirrison7@gmail.com>
2025-08-29 19:10:12 -0300

    Atividade 15 resolvida

--- AtividadesJava/src/Atividade15.java
+++ AtividadesJava/src/Atividade15.java
@@ -1,7 +1,11 @@
 public class Atividade15 {
     public static void main(String[] args) {
         int x = 10;
-        
+        x = x += 5;
+        x = x *= 2;
+        x = x -= 4;
+        System.out.println("O resultado final de x é: "+x);
+
 
     }
 }
